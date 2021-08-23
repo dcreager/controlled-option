29e44530b493dd01cd4e62d6f7d0c1da848b563b
Douglas Creager <dcreager@dcreager.net>
2021-08-23 19:24:49 -0400

    Add Niche implementation for a test struct
    This struct has two fields that have niches available, and the ‘Niche’
    implementation explicitly chooses to use the second one.  I've been
    careful to write the implementation so that it doesn't need to refer to
    the field's type by name.  That will make it easier to write a proc
    macro to implement this automatically for a struct.

--- tests/it/main.rs
+++ tests/it/main.rs
@@ -5,7 +5,10 @@
 // Please see the LICENSE-APACHE or LICENSE-MIT files in this distribution for license details.
 // ------------------------------------------------------------------------------------------------
 
+use std::mem::MaybeUninit;
 use std::num::NonZeroU32;
+use std::ptr::addr_of;
+use std::ptr::addr_of_mut;
 
 use controlled_option::ControlledOption;
 use controlled_option::Niche;
@@ -36,4 +39,91 @@ fn can_option_nonzeros() {
     assert!(some.is_some());
     // `Some` non-zero values should be represented by themselves.
     assert_eq!(NonZeroU32::from_some(75), NonZeroU32::new(75).unwrap());
+}
+
+// This is a struct that has two fields that have niche values available.  We'll explicitly choose
+// to use the one from the second field as the niche for the struct as a whole.
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+struct TestStruct {
+    a: NonZeroU32,
+    b: NonZeroU32,
+}
+
+impl TestStruct {
+    fn new(a: u32, b: u32) -> TestStruct {
+        TestStruct {
+            a: NonZeroU32::new(a).unwrap(),
+            b: NonZeroU32::new(b).unwrap(),
+        }
+    }
+}
+
+fn fill_field_with_none<T>(field: *mut T)
+where
+    T: Niche,
+{
+    debug_assert!(std::alloc::Layout::new::<T>() == std::alloc::Layout::new::<T::Output>());
+    let repr = field as *mut T::Output;
+    unsafe { repr.write(T::none()) };
+}
+
+fn field_is_none<T>(field: *const T) -> bool
+where
+    T: Niche,
+{
+    debug_assert!(std::alloc::Layout::new::<T>() == std::alloc::Layout::new::<T::Output>());
+    let repr = field as *const T::Output;
+    T::is_none(unsafe { &*repr })
+}
+
+impl Niche for TestStruct {
+    type Output = MaybeUninit<Self>;
+
+    #[inline]
+    fn none() -> Self::Output {
+        let mut value = Self::Output::uninit();
+        let ptr = value.as_mut_ptr();
+        fill_field_with_none(unsafe { addr_of_mut!((*ptr).b) });
+        value
+    }
+
+    #[inline]
+    fn is_none(value: &Self::Output) -> bool {
+        let ptr = value.as_ptr();
+        field_is_none(unsafe { addr_of!((*ptr).b) })
+    }
+
+    #[inline]
+    fn into_some(value: Self) -> Self::Output {
+        MaybeUninit::new(value)
+    }
+
+    #[inline]
+    fn from_some(value: Self::Output) -> Self {
+        unsafe { value.assume_init() }
+    }
+}
+
+#[repr(C)]
+#[derive(Debug)]
+struct TestStructRepr {
+    a: u32,
+    b: u32,
+}
+
+#[test]
+fn can_option_structs() {
+    let none = ControlledOption::<TestStruct>::none();
+    assert!(none.is_none());
+    let none_repr: TestStructRepr = unsafe { std::mem::transmute(none) };
+    assert_eq!(none_repr.b, 0);
+
+    let value = TestStruct::new(75, 125);
+    let some = ControlledOption::some(value);
+    assert!(some.is_some());
+    let some_repr: TestStructRepr = unsafe { std::mem::transmute(some) };
+    assert_eq!(some_repr.a, 75);
+    assert_eq!(some_repr.b, 125);
 }
